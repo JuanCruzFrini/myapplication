db96d89c882368ee2353dc893fbd0ae07b4f97e8
JF_E91165 <JF_E91165@redlink.com.ar>
2024-01-17 09:51:28 -0300

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/ImagePreview.kt
@@ -0,0 +1,15 @@
+package com.example.mylibrary
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ImagePreview(){
+    Box(modifier = Modifier.size(80.dp).background(Color.Blue))
+}
